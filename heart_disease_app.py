12129b7a5e07a0f0c8b87b7083147c1bd9eabbab
vikas_yadav <146157557+gethubvikas123@users.noreply.github.com>
2024-03-24 19:08:20 +0530

    heart_disease_app.py

--- heart_disease_app.py
+++ heart_disease_app.py
@@ -5,7 +5,7 @@ import streamlit as st
 
 
 
-model = joblib.load(r'heart_disease_prediction_model.joblib', mmap_mode=None)
+model = joblib.load(r'heart_disease_prediction_model.joblib')
 
 
 def predict_heart_disease(input_data):
